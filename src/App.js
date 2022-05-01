b0d0ecc9b7c691e18734a5eb94ac8d06d51c7490
Shivam nagar <nagarshivam409@gmail.com>
2022-05-01 17:13:06 +0530

    React-TextUtils
    This is React based Web Application which can be used to manipulated your text.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar'
+import Textform from './components/Textform';
+import React, { useState } from 'react';
+import Alert from './components/Alert'
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+  
+// } from "react-router-dom";
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState('null');
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert('null')
+    }, 1500);
+  }
+  
+  const toggle=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#040420';
+      showAlert("Dark Mode Enabled","success");
+      document.title="TextUtils -DarkMode";
+      
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode Enabled","success");
+      document.title="TextUtils -LightMode";
+      
+      
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   {/* <Router> */}
+    <Navbar title="TextUtils" mode={mode} toggle={toggle}/>
+    <Alert alert={alert}/>
+    {/* <Switch>
+        <Route exact path="/"> */}
+          <div className="container my-3">
+            <Textform heading ="Enter Your Text Below to Analyze" mode={mode} showAlert={showAlert} toggle={toggle} />
+          </div>
+          {/* </Route>
+          <Route exact path="/about">
+           */}
+              {/* <About/>
+            
+          </Route> */}
+          {/* <Route path="/home">
+          <div className="container my-3">
+            <Textform heading ="Enter Your Text Below to Analyze" mode={mode} showAlert={showAlert} toggle={toggle} />
+          </div>
+          </Route> */}
+          
+        {/* </Switch>
+    
+        </Router> */}
+    </>
   );
 }
 
